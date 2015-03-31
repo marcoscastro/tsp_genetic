0025c589bab94a6d1ff8ba585f655f8473aef112
marcos <marcos@marcos-not.(none)>
2015-03-31 20:16:21 -0300

    added images of graphs

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ int main()
 	
 	// creates the graph1 with parameters: number of vertexes and initial vertex
 	Graph * graph1 = new Graph(5, 0);
-	
 	// add edges
 	graph1->addEdge(0, 1, 1);
 	graph1->addEdge(1, 0, 1);
@@ -33,7 +32,6 @@ int main()
 	
 	// creates the graph2
 	Graph * graph2 = new Graph(4, 0);
-	
 	// add edges
 	graph2->addEdge(0, 1, 2);
 	graph2->addEdge(1, 0, 2);
@@ -47,6 +45,22 @@ int main()
 	graph2->addEdge(2, 1, 4);
 	graph2->addEdge(2, 3, 2);
 	graph2->addEdge(3, 2, 2);
+	
+	// creates the graph3
+	Graph * graph3 = new Graph(4, 0);
+	// add edges
+	graph3->addEdge(0, 1, 10);
+	graph3->addEdge(1, 0, 10);
+	graph3->addEdge(1, 3, 20);
+	graph3->addEdge(3, 1, 20);
+	graph3->addEdge(0, 2, 15);
+	graph3->addEdge(2, 0, 15);
+	graph3->addEdge(1, 3, 25);
+	graph3->addEdge(3, 1, 25);
+	graph3->addEdge(2, 3, 30);
+	graph3->addEdge(3, 2, 30);
+	graph3->addEdge(1, 2, 35);
+	graph3->addEdge(2, 1, 35);
 	
 	// parameters: the graph, population size, iterations and mutation rate
 	Genetic genetic(graph1, 50, 100, 5);
